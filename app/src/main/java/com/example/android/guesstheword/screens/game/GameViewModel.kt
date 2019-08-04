6d8483e38e30462de56fc5f6538960a22d1dde09
bschiranth <bschiranth@gmail.com>
2019-08-04 18:57:07 -0500

    adding view model dependency

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.android.guesstheword.screens.game
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+
+    override fun onCleared() {
+        super.onCleared()
+
+    }
+}
